4f32f05002d001a4c62ce659365d466c700df171
Gergo Czifra <gergoczifra91@gmail.com>
2021-07-12 10:05:29 +0200

    started unit tests

--- src/test/java/katas/Kata1Test.java
+++ src/test/java/katas/Kata1Test.java
@@ -1,16 +1,29 @@
 package katas;
 
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
 import org.hamcrest.CoreMatchers;
+
 import static org.hamcrest.CoreMatchers.*;
 
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.List;
+import java.util.Map;
 
 
 public class Kata1Test {
 
     @Test
     public void testExecute() {
-        Assert.assertThat(Kata1.execute().size(), equalTo(4));
+        Assert.assertThat(Kata1.execute(), equalTo(generateTestData()));
+    }
+
+    private List<Map> generateTestData() {
+        return ImmutableList.of(ImmutableMap.of("id", 70111470, "title", "Die Hard"),
+                ImmutableMap.of("id", 654356453, "title", "Bad Boys"),
+                ImmutableMap.of("id", 65432445, "title", "The Chamber"),
+                ImmutableMap.of("id", 675465, "title", "Fracture"));
     }
 }
--- src/test/java/katas/Kata2Test.java
+++ src/test/java/katas/Kata2Test.java
@@ -1,7 +1,10 @@
 package katas;
 
+import com.google.common.collect.ImmutableList;
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 
@@ -10,6 +13,10 @@ public class Kata2Test {
 
     @Test
     public void testExecute() {
-        Assert.assertThat(Kata2.execute().size(), equalTo(4));
+        Assert.assertThat(Kata2.execute(), equalTo(generateTestData()));
+    }
+
+    private List<Integer> generateTestData() {
+        return ImmutableList.of(654356453, 675465);
     }
 }
